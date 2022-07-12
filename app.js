9fc63e0c267903b6235c1c00fb8778c579d34ef9
Wilbax <janithravisankax@gmail.com>
2022-07-12 18:43:12 +0530

    v 1.0

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs =  require("ejs");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+mongoose.connect("mongodb+srv://Admin-janith:pJwACPwWsYaNORYp@cluster0.cl5pp.mongodb.net/itemDB");
+
+
+app.get("/", function(req, res){
+    res.render("header")
+});
+
+app.get("/about", function(req, res){
+    res.render("about")
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact")
+});
+
+app.get("/services", function(req, res){
+ res.render("service")
+});
+
+app.get("/:cname",function(req, res){
+    res.render("items", ({title:req.params.cname}))
+});
+
+app.listen(3000, function(){
+    console.log("sever successfully running on port 3000")
+});
---
+++ readme.txt
@@ -0,0 +1,2 @@
+This if a full stack website based on node.js
+if u wanna start the server, install node first
